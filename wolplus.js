46dce18095d02a85a8d54997075e85a09e4bdbf9
Amio <amio.cn@gmail.com>
2012-10-30 19:28:49 +0800

    + 从 Gist 迁移至 Github Repo

---
+++ wolplus.js
@@ -0,0 +1,48 @@
+(function () {
+
+	/**
+	 * Inject translation
+	 */
+	(function(){
+		if (window['$WowheadPower']) {
+				transTip(window['$WowheadPower']);
+			} else {
+				setTimeout(arguments.callee,200);
+			}
+	})();
+
+	var transTip = function ($Whp) {
+
+		function inject(script) {
+			var head = document.querySelector('head');
+			var el = document.createElement('script');
+			el.setAttribute("src", script);
+			head.appendChild(el); // Run the 'spell' script from 178.com
+			head.removeChild(el); // Clean up
+		}
+
+		// save original $WowheadPower.registerSpell
+		var origReg = $Whp.registerSpell;
+
+		// Set $187DB enviroment
+		window.$178DB = {
+			'spells': {},
+			'regstSpell': function (cnSpellObj) {
+				var currentSpell = $178DB.spells[cnSpellObj['id']];
+				//replace en tips with cn tips
+				currentSpell[2]['tooltip_enus'] = cnSpellObj['tip'];
+				//call the origin register
+				origReg.apply($Whp, currentSpell);
+			}
+		};
+
+		// New $WowheadPower.registerSpell as a bridge
+		$Whp.registerSpell = function (id, zero, spellObj) {
+			var cnSpellJs = 'http://db.178.com/wow/cn/a/spell/{id}.js'.replace('{id}', id);
+			$178DB['spells'][id] = [id, zero, spellObj];
+			// OK, It's your turn now
+			inject(cnSpellJs);
+		};
+	}
+
+})();
---
+++ wolplus.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         WOL.Plus
+// @namespace    http://www.airmio.com/
+// @version      0.2
+// @description  将WOL中鼠标指向技能的英文说明替换为中文说明
+// @match        http://www.worldoflogs.com/*
+// @updateURL    https://
+// @downloadURL  https://gist.github.com/raw/3930081/wolcn.user.js
+// @run-at       document-start
+// ==/UserScript==
+
+;'use strict';
+(function(){
+
+	// List to inject
+	var injects = [
+		'script>https://github.com/amio/wolcn/raw/master/wolplus.js',
+		'style>https://github.com/amio/wolcn/raw/master/wolplus.css'
+	];
+
+	var head = document.querySelector('head'),
+		TMPL = {
+			script: '<script type="text/javascript" src="{$src}"></script>',
+			style : '<link rel="stylesheet" type="text/css" href="{$src}" />'
+		};
+
+	if (head) {
+		var fragment = document.createElement('div');
+		for(var i = injects.length, item; i--;){
+			item = injects[i].split('>');
+			fragment.innerHTML = TMPL[item[0]].replace('{$src}',item[1]);
+			head.appendChild(fragment.firstChild);
+		}
+	}
+})();
